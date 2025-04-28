d2b91c8dc410d491d33d2bb3bae6337d7dc43ec4
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-04-29 01:10:39 +0300

    entity eklendi tema eklendi api testleri yapıldı

---
+++ SignalRApi/Controllers/BookingController.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.EntiyLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+
+
+        public BookingController(IBookingService bookingService, IMapper mapper)
+        {
+            _bookingService = bookingService;
+        }
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateBooking(CreateBookingDto createBookingDto)
+        {
+            Booking booking = new Booking
+            {
+                Name = createBookingDto.Name,
+                Phone = createBookingDto.Phone,
+                Mail = createBookingDto.Mail,
+                PersonCount = createBookingDto.PersonCount,
+                Date = createBookingDto.Date,
+                Description = createBookingDto.Description
+            };
+            _bookingService.TAdd(booking);
+            return Ok("Rezervasyon Yapıldı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            _bookingService.TDelete(value);
+            return Ok("Rezervasyon Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBooking(UpdateBookingDto updateBookingDto)
+        {
+            Booking booking = new Booking
+            {
+                BookingID = updateBookingDto.BookingID,
+                Name = updateBookingDto.Name,
+                Phone = updateBookingDto.Phone,
+                Mail = updateBookingDto.Mail,
+                PersonCount = updateBookingDto.PersonCount,
+                Date = updateBookingDto.Date,
+                Description = updateBookingDto.Description
+            };
+            _bookingService.TUpdate(booking);
+            return Ok("Rezervasyon Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            return Ok(value);
+        }  
+    }
+}
