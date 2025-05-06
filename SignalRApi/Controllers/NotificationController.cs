e1a7d5a5c0eef22efc0988df7260c1d458ee5996
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-05-06 22:01:22 +0300

    Anlık bildirim işlemleri tamamlandı Masa boş dolu durumu yapılacak

---
+++ SignalRApi/Controllers/NotificationController.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+        [HttpGet]
+        public IActionResult NotificationList()
+        {
+            return Ok(_notificationService.TGetListAll());
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountByStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+
+        [HttpGet("GetAllNotificationByFalse")]
+        public IActionResult GetAllNotificationByFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByFalse());
+        }
+
+        [HttpPost]
+        public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification
+            {
+                Type = createNotificationDto.Type,
+                Icon = createNotificationDto.Icon,
+                Description = createNotificationDto.Description,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Status = createNotificationDto.Status
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Ekleme işlemi başarıyla yapıldı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim Silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetByID(id);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification = new Notification
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Type = updateNotificationDto.Type,
+                Icon = updateNotificationDto.Icon,
+                Description = updateNotificationDto.Description,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString()),
+                Status = updateNotificationDto.Status
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncelleme işlemi başarıyla yapıldı");
+        }
+
+        [HttpGet("NotificationStatusChangeToFalse/{id}")]
+        public IActionResult NotificationStatusChangeToFalse(int id)
+        {
+            _notificationService.TNotificationStatusChangeToFalse(id);
+            return Ok("Güncelleme yapıldı");
+        }
+
+        [HttpGet("NotificationStatusChangeToTrue/{id}")]
+        public IActionResult NotificationStatusChangeToTrue(int id)
+        {
+            _notificationService.TNotificationStatusChangeToTrue(id);
+            return Ok("Güncelleme yapıldı");
+        }
+    }
+}
--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -12,7 +12,8 @@ namespace SignalRApi.Hubs
         private readonly IMoneyCaseService _moneyCaseService;
         private readonly IMenuTableService _menuTableService;
         private readonly IBookingService _bookingService;
-        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService)
+        private readonly INotificationService _notificationService;
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService, INotificationService notificationService)
         {
             _categoryService = categoryService;
             _productService = productService;
@@ -20,6 +21,7 @@ namespace SignalRApi.Hubs
             _moneyCaseService = moneyCaseService;
             _menuTableService = menuTableService;
             _bookingService = bookingService;
+            _notificationService = notificationService;
         }
 
         public static int clientCount { get; set; } = 0;
@@ -119,6 +121,15 @@ namespace SignalRApi.Hubs
             clientCount--;
             await Clients.All.SendAsync("ReceiveClientCount", clientCount);
             await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendNotification()
+        {
+            var value = _notificationService.TNotificationCountByStatusFalse();
+            await Clients.All.SendAsync("ReceiveNotificationCountByFalse", value);
+
+            var notifications = _notificationService.TGetAllNotificationByFalse();
+            await Clients.All.SendAsync("ReceiveNotificationListByFalse", notifications);
         }
     }
 }
